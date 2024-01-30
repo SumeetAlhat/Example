a6db7fc9abaca0168958f5ac2628bd1fb27d85e1
SumeetAlhat <SumeetAlhat@LP-IN356010>
2024-01-30 15:12:33 +0530

    Squash commit

--- Example.cpp
+++ Example.cpp
@@ -8,5 +8,17 @@ int main()
     std::cout << "Hello World Spirangle Buddies. How aare you doing? \n";
     std::cout << "Hello World Spirangle Buddies. How aare you doing? \n";
     std::cout << "Hello World Spirangle Buddies. How aare you doing? \n";
+
+    std::cout << "Reset";
 }
 
+void method1() {
+    std::cout << "method 1 \n";
+}
+
+void method2() {
+    std::cout << "method 2 \n";
+}
+void method3() {
+    std::cout << "method 2 \n";
+}
